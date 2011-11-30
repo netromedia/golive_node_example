35b73e3ea85393607bf3e33054d82960d5dec446
William Hetherington <will@netromedia.com>
2011-11-30 10:10:31 -0800

    Removed JSON.Stringify

--- GoLive.js
+++ GoLive.js
@@ -3,7 +3,7 @@ var request = require('request')
     { method: 'POST'
     , uri: 'https://login.netromedia.com/rest.svc/login'
     , json: true
-    ,  body: JSON.stringify({Email: 'me@myemail.com', Password: "PassworD"})
+    ,  body: {Email: 'me@myemail.com', Password: 'PassworD'}
     }
   , function (error, response, body) {
       if(response.statusCode == 200){
